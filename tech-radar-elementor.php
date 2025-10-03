f6850d3c9aa8071872296b8a15bd4b988d54c8a8
fuadadhim24 <fuadadhim24@gmail.com>
2025-10-03 14:51:36 +0700

    feat: add Elementor Tech Radar widget integration

---
+++ tech-radar-elementor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Elementor Tech Radar Widget
+ * Description: Custom Elementor widget untuk menampilkan Tech Radar dengan data configurable (date + entries).
+ * Version: 1.0
+ * Author: Fuad
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_action( 'elementor/widgets/register', function( $widgets_manager ) {
+    require_once __DIR__ . '/widget-tech-radar.php';
+    $widgets_manager->register( new \Elementor_Tech_Radar_Widget() );
+} );
+
+add_action( 'wp_enqueue_scripts', function() {
+    wp_enqueue_script( 'd3-js', 'https://d3js.org/d3.v7.min.js', [], null, true );
+} );
